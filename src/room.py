d73cc46025c7d8bbe954c72b297f06b6f0235a32
ying <huaiy1105@gmail.com>
2023-07-14 23:47:04 +0100

    test check out guest

--- src/room.py
+++ src/room.py
@@ -8,11 +8,10 @@ class Room:
 
     def check_in_guest(self, guest):
         self.new_guest.append(guest)
-        return self.new_guest
     
     def check_out_guest(self, guest):
-        self.new_guest.remove(guest)
-        return self.new_guest
+        if guest in self.new_guest:
+            self.new_guest.remove(guest)
     
     def add_songs(self, song):
         self.play_list.append(song.name)
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,13 +1,14 @@
 import unittest
 from src.room import Room
 from src.song import Song
+from src.guest import Guest
 class TestRoom(unittest.TestCase):
     def setUp(self):
-        self.guest_1 = Room("Ying")
-        self.guest_2 = Room("Patrick")
+        self.guest_1 = Guest("Ying", 100)
+        self.guest_2 = Guest("Patrick", 80)
         self.room_1 = Room("Room 1")
         self.song = Song("Makeba")
-        # self.room_1.new_guest = [self.guest_1]
+        self.room_1.new_guest = []
 
     def test_room_has_name(self):
         self.assertEqual("Room 1", self.room_1.name)
@@ -16,9 +17,11 @@ class TestRoom(unittest.TestCase):
         self.room_1.check_in_guest(self.guest_1.name)
         self.assertEqual(["Ying"], self.room_1.new_guest)
     
-    # def test_room_check_out_guest(self):
-    #     self.room_1.check_out_guest(self.guest_2.name)
-    #     self.assertEqual(["Patrick"], self.room_1.new_guest)
+    def test_room_check_out_guest(self):
+        guest = Guest("Craig", 200)
+        self.room_1.new_guest = ["Craig"]
+        self.room_1.check_out_guest(guest.name)
+        self.assertEqual([], self.room_1.new_guest)
 
     def test_has_add_song_to_room(self):
         self.room_1.add_songs(self.song)    
